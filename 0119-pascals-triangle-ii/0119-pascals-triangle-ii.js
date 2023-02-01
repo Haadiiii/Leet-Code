b530914408bc6f4e542e2db92bd42771ba33f526
Hamid Ali <hammali99@gmail.com>
2023-02-01 23:59:47 +0500

    Time: 67 ms (62.58%), Space: 41.7 MB (81.44%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -6,7 +6,7 @@ var getRow = function(rowIndex) {
     let result = []
     
     for(let i=0; i<= rowIndex; i++){
-        let row = new Array(i + 1)
+        let row = [i+1]
         row[0] = 1
         row[i] = 1
         
@@ -17,4 +17,27 @@ var getRow = function(rowIndex) {
         result.push(row)
     }
     return result[rowIndex];
+   //  let result = []
+   //  if(rowIndex >= 0) result.push([1])
+   //  if(rowIndex >= 1) result.push([1,1])
+   //  console.log(result)
+   //  for(let i = 1; i <= rowIndex; i++){
+   //      let firstNum = 1
+   //      let lastNum = 1
+   //      let prevArr = result[i-1]
+   //      if(prevArr.length === 1){
+   //          result.push([firstNum,firstNum + lastNum,lastNum ])
+   //      }else{
+   //          let left = 0
+   //          let right = 1 
+   //          let add = []
+   //          while(right < prevArr.length){
+   //              add.push(prevArr[left] + prevArr[right])
+   //              left++
+   //              right++
+   //          }
+   //          result.push([firstNum, ...add, lastNum])
+   //      }
+   //  }
+   // return (result[rowIndex]);
 };
