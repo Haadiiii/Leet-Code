7056c2aa46e389d27f0cbb7bcc124b8e0ae693e5
Hamid Ali <hammali99@gmail.com>
2023-02-28 20:34:53 +0500

    Time: 107 ms (48.72%), Space: 55.4 MB (18.49%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -10,12 +10,21 @@ var containsDuplicate = function(nums) {
    //      seen.add(num)
    //  return False
     
-    let seen = new Set();
-    for (let num of nums) {
-        if (seen.has(num)) {
-            return true;
+    // let seen = new Set();
+    // for (let num of nums) {
+    //     if (seen.has(num)) {
+    //         return true;
+    //     }
+    //     seen.add(num);
+    // }
+    // return false;
+    
+        let seen = new Set()
+    for(let num of nums){
+        if(seen.has(num)){
+            return true
         }
-        seen.add(num);
+        seen.add(num)
     }
-    return false;
+    return false
 };
